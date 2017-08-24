7c2d6c567f2b7948e5d593614c64dce7dce290ab
Anton Uspenskiy <antuspenskiy@gmail.com>
2017-08-24 18:46:18 +0300

    add rotation for dumps and synchronization via rsync, use pretty json

--- dbdump/dbdump.go
+++ dbdump/dbdump.go
@@ -5,28 +5,31 @@ import (
 	"bytes"
 	"compress/gzip"
 	"io/ioutil"
-	"log"
 	"flag"
 	"time"
 	"os"
 	"fmt"
+
+	"github.com/antuspenskiy/automate-vhosts/branch"
 )
 
 // Set the command line arguments
 var (
-	mysqlUser   = flag.String("db-user", "test", "Name of your MySql dbdump USER")
-	mysqlHost   = flag.String("db-host", "localhost", "Name of your MySql dbdump HOST")
-	mysqlDb     = flag.String("db", "i_66chucknorris", "Database Name")
-	allDatabase = flag.Bool("dbdump-all", false, "If set script dbdump all MySql Databases")
-	tmpDir      = flag.String("tmp-dir", "/tmp", "Temp directory (default /tmp)")
-	logDir      = flag.String("log-dir", "/tmp", "Log directory (default /var/log)")
-	gzipEnable  = flag.Bool("gzip", true, "If set Gzip Compression Enabled")
+	mysqlUser     = flag.String("u", "test", "Name of your database user.")
+	mysqlPassword = flag.String("p", "test", "Password of your database user.")
+	mysqlHost     = flag.String("h", "localhost", "Name of your Mysql hostname.")
+	mysqlDb       = flag.String("db", "test", "Database name.")
+	allDatabase   = flag.Bool("db-all", false, "If set dump all Mysql databases.")
+	backupDir     = flag.String("backup-dir", "/opt/backup/db", "Backup directory for dumps.")
+	storageDir    = flag.String("storage-dir", "/mnt/backup", "Remote storage directory for dumps.")
+	gzipEnable    = flag.Bool("gzip", true, "If set gzip compression enabled.")
 )
 
 func main() {
 
 	// Get command line arguments
 	flag.Parse()
+	flag.Args()
 
 	// Get the hostname
 	hostname, err := os.Hostname()
@@ -35,22 +38,13 @@ func main() {
 	current := time.Now()
 	now := fmt.Sprintf(current.Format("20060102.150405"))
 
-	// Open the log file
-	ol, err := os.OpenFile(*logDir+"/go-mysql-dbdump.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer ol.Close()
-
-	log.SetOutput(ol)
-
 	// Set Filename
 	if *allDatabase {
-		log.Println("Dump " + hostname + " DBs Start")
-		filename = hostname + "_" + now + ".sql"
+		fmt.Printf("Output: Dumping %s databases's start...\n", hostname)
+		filename = fmt.Sprintf("%s_%s.sql", hostname, now)
 	} else {
-		log.Println("Dump DB " + *mysqlDb + " Start")
-		filename = *mysqlDb + "_" + now + ".sql"
+		fmt.Printf("Output: Dumping database %s start...\n", *mysqlDb)
+		filename = fmt.Sprintf("%s_%s.sql", *mysqlDb, now)
 	}
 
 	if *gzipEnable {
@@ -58,25 +52,26 @@ func main() {
 	}
 
 	// Define local tmp file
-	localTmpFile := *tmpDir + "/" + filename
+	localTmpFile := fmt.Sprintf("%s/%s", *backupDir, filename)
 
 	// Compose mysqldump command
-	mysqldumpCommand := "mysqldump --single-transaction -u " + *mysqlUser + " -h " + *mysqlHost + " "
+	mysqldumpCommand := fmt.Sprintf("mysqldump -u%s -p%s -h%s --single-transaction ", *mysqlUser, *mysqlPassword, *mysqlHost)
 	if *allDatabase {
 		mysqldumpCommand += "--all-databases "
 	} else if *mysqlDb != "" {
 		mysqldumpCommand += *mysqlDb
 	} else {
-		log.Fatal("You must specify a DB Name")
+		fmt.Println("You must specify a database name")
 	}
 
-	// Create database dbdump and store it on local tmp file
+	// TODO: Refactor to ExecCmd fun or similar
+	// Create database dump and store it on local tmp file
 	cmd := exec.Command("/bin/bash", "-c", mysqldumpCommand)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal("Mysqldump Error", err)
+		fmt.Printf("Error: %v\n", err)
 	}
 
 	// Create a gzip file of the dbdump output stream
@@ -91,6 +86,14 @@ func main() {
 
 	// Write the gzip stream to a tmp file
 	ioutil.WriteFile(localTmpFile, out.Bytes(), 0666)
+	fmt.Printf("Output: Gzip file %s created\n", localTmpFile)
 
-	log.Println("Dump DB " + *mysqlDb + " Finish")
+	// TODO: Better to use semicolon for rm {} \;
+	// Rotate dumps then synchronize it via rsync
+	branch.ExecCmd("bash", "-c", fmt.Sprintf("find %s/ -name '*.sql.gz' -type f -mtime +14 -exec rm {} +", *backupDir))
+
+	// Synchronize backup directory with storage directory
+	branch.ExecCmd("bash", "-c", fmt.Sprintf("rsync -avpze --progress --stats --delete %s/ %s/", *backupDir, *storageDir))
+
+	fmt.Printf("Output: Dump database %s finished.\n", *mysqlDb)
 }
