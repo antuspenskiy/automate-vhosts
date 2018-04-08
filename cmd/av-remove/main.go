618268aaf3adbe9b5f91b403d330081363055dc0
Anton Uspenskiy <antuspenskiy@gmail.com>
2018-04-08 22:28:34 +0300

    step to refactor structure

--- cmd/av-remove/main.go
+++ cmd/av-remove/main.go
@@ -9,7 +9,9 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/antuspenskiy/automate-vhosts/pkg/branch"
+	"github.com/antuspenskiy/automate-vhosts/pkg/cmd"
+	"github.com/antuspenskiy/automate-vhosts/pkg/config"
+	"github.com/antuspenskiy/automate-vhosts/pkg/db"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -44,11 +46,11 @@ func main() {
 	flag.Parse()
 
 	// Load json configuration
-	conf, err := branch.ReadConfig("env")
-	branch.Check(err)
+	conf, err := config.ReadConfig("env")
+	cmd.Check(err)
 
 	// Get server hostname
-	hostname := branch.GetHostname()
+	hostname := cmd.GetHostname()
 
 	// Variables
 	hostDir := conf.GetString("rootdir") + *refSlug
@@ -56,16 +58,16 @@ func main() {
 
 	// List remote branches, only 2nd row without refs/heads/
 	err = os.Chdir(hostDir)
-	branch.Check(err)
+	cmd.Check(err)
 	gitlsRemote, err := exec.Command("bash", "-c", "sudo -u user git ls-remote --heads origin | awk '{print $2}' | sed 's/.*\\/\\(.*\\).*/\\1/'").CombinedOutput()
-	branch.Check(err)
+	cmd.Check(err)
 	fmt.Printf("\nRemote Branches:\n\n%s\n", gitlsRemote)
 
 	// List folders
 	err = os.Chdir(conf.GetString("rootdir"))
-	branch.Check(err)
+	cmd.Check(err)
 	lsFolder, err := exec.Command("bash", "-c", "ls -d */ | grep -v 'pm2json\\|log\\|intranet\\|default' | cut -f1 -d'/'").CombinedOutput()
-	branch.Check(err)
+	cmd.Check(err)
 	fmt.Printf("Branches Folders:\n\n%s\n", lsFolder)
 
 	// Convert []byte to string and split string to []string
@@ -76,24 +78,24 @@ func main() {
 	lsFolderStrSplit := strings.Split(lsFolderStr, "\n")
 
 	// Use difference function
-	diffStr := branch.Difference(lsFolderStrSplit, gitlsRemoteStrSplit)
+	diffStr := cmd.Difference(lsFolderStrSplit, gitlsRemoteStrSplit)
 
 	// Connect to MySQL
 	// [user[:pass]@][protocol[(addr)]]/dbname[?p1=v1&...]
 	mysqlInfo := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		*mysqlUser, *mysqlPassword, *mysqlHostname, *mysqlPort, *mysqlDatabase)
 
-	db, err := sql.Open("mysql", mysqlInfo)
+	conn, err := sql.Open("mysql", mysqlInfo)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer func() {
-		err = db.Close()
-		branch.Check(err)
+		err = conn.Close()
+		cmd.Check(err)
 	}()
 
 	// make sure connection is available
-	err = db.Ping()
+	err = conn.Ping()
 	if err != nil {
 		log.Fatalf(err.Error())
 	} else {
@@ -105,40 +107,40 @@ func main() {
 		fmt.Printf("This folder and settings will be deleted:\n%s\n\n", diffVal)
 
 		// dbName not equal diffVal we need to parse this values in ParseBranchName
-		dbName := branch.ParseBranchName(diffVal)
+		dbName := db.ParseBranchName(diffVal)
 
 		// Delete MySQL database
 
-		numdrop, err := branch.DropDB(db, dbName)
-		branch.Check(err)
+		numdrop, err := db.DropDB(conn, dbName)
+		cmd.Check(err)
 		log.Printf("MySQL: Running: DROP DATABASE IF EXISTS %s;\n", dbName)
 		log.Printf("MySQL: Query OK, %d rows affected\n\n", numdrop)
 
-		numdropuser, err := branch.DropUser(db, dbName)
-		branch.Check(err)
+		numdropuser, err := db.DropUser(conn, dbName)
+		cmd.Check(err)
 		log.Printf("MySQL: Running: DROP USER '%s'@'localhost';\n", dbName)
 		log.Printf("MySQL: Query OK, %d rows affected\n\n", numdropuser)
 
-		numflush, err := branch.FlushPriv(db)
-		branch.Check(err)
+		numflush, err := db.FlushPriv(conn)
+		cmd.Check(err)
 		log.Printf("MySQL: Running: FLUSH PRIVILEGES;")
 		log.Printf("MySQL: Query OK, %d rows affected\n\n", numflush)
 
 		// Remove virtual host directory
-		branch.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s", diffVal))
+		cmd.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s", diffVal))
 
 		// Remove nginx configuration file for virtual host
-		branch.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s/%s.conf", conf.GetString("nginxdir"), diffVal))
+		cmd.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s/%s.conf", conf.GetString("nginxdir"), diffVal))
 
 		// Remove php-fpm configuration file for virtual host
-		branch.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s/%s.conf", conf.GetString("fpmdir"), diffVal))
+		cmd.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s/%s.conf", conf.GetString("fpmdir"), diffVal))
 
 		if strings.Contains(hostname, "intranet") {
 			// Remove pm2 process and configuration file for virtual host
-			branch.RunCommand("bash", "-c", fmt.Sprintf("sudo -u user pm2 delete --silent %s", diffVal))
-			branch.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s/%s.json", pm2Dir, diffVal))
+			cmd.RunCommand("bash", "-c", fmt.Sprintf("sudo -u user pm2 delete --silent %s", diffVal))
+			cmd.RunCommand("bash", "-c", fmt.Sprintf("rm -fr %s/%s.json", pm2Dir, diffVal))
 		}
 	}
 	// Restart nginx and php-fpm
-	branch.RunCommand("bash", "-c", "systemctl restart nginx php-fpm")
+	cmd.RunCommand("bash", "-c", "systemctl restart nginx php-fpm")
 }
